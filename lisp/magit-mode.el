cdeb6fd08ef7a10e3360693a1c601a620b6bf995
Jonas Bernoulli <jonas@bernoul.li>
2019-04-08 09:01:00 +0200

    magit-revision-mode: Implement magit-buffer-value

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -1022,9 +1022,6 @@ See also `magit-toggle-buffer-lock'.")
                           'unstaged))
                     const)
               (and files (cons "--" files)))))
-    (magit-revision-mode
-     (pcase-let ((`(,rev ,_const ,_args ,files) args))
-       (if files (cons rev files) (list rev))))
     (t
      (--when-let (cdr (assq mode magit-buffer-lock-functions))
        (funcall it args)))))
